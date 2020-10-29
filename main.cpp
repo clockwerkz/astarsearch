4de5e3f5665a83f7101d28722e457fa4440e2d62
Carlos <clockwerkz@gmail.com>
2020-10-29 14:08:00 -0700

    adds read/print board functionality

--- main.cpp
+++ main.cpp
@@ -1,25 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <string>
 
-using std::cout;
-using std::string;
-using std::vector;
+#include "main.h"
 
-enum class State
+vector<State> ParseLine(string line)
 {
-    kEmpty,
-    kObstacle
-};
+    vector<State> row;
+    int num;
+    char c;
+    std::istringstream myStream(line);
+    while (myStream >> num >> c && c == ',')
+    {
+        row.push_back(num == 0 ? State::kEmpty : State::kObstacle);
+    }
+    return row;
+}
 
 vector<vector<State>> ReadBoardFile(string path)
 {
-    cout << "Loading the board file: " << path << std::endl;
-    return vector<vector<State>>{};
+    std::fstream fileName(path);
+    if (!fileName)
+    {
+        cout << "There was an error loading the file.\n";
+        return vector<vector<State>>{};
+    }
+    string line;
+    vector<vector<State>> board;
+    while (getline(fileName, line))
+    {
+        board.push_back(ParseLine(line));
+    }
+    return board;
+}
+
+string CellString(State cell)
+{
+    switch (cell)
+    {
+    //case State::kObstacle: return "⛰️   ";
+    case State::kObstacle:
+        return "X   ";
+    default:
+        return "0   ";
+    }
+}
+
+void PrintBoard(vector<vector<State>> board)
+{
+    for (auto row : board)
+    {
+        for (auto cell : row)
+        {
+            cout << CellString(cell);
+        }
+        cout << std::endl;
+    }
 }
 
 int main()
 {
     auto board = ReadBoardFile("1.board");
+    PrintBoard(board);
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::cout;
+using std::string;
+using std::vector;
+
+enum class State
+{
+    kEmpty,
+    kObstacle
+};
