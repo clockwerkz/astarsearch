ca010141c3a5655330ff9609e578efad2385f3b0
Carlos <clockwerkz@gmail.com>
2020-11-01 19:32:01 -0800

    adding in some practice wih ref and pointers

--- playground/pointers.cpp
+++ playground/pointers.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
 using std::cout;
+using std::vector;
 
-
-void PointerTest(int *a) {
+void PointerTest(int *a)
+{
     cout << "This is the memory address of pointer: " << a << std::endl;
     cout << "This is the int value of pointer: " << *a << std::endl;
     // accessing the value inside the memory location held by variable a
-    *a = 6; 
+    *a = 6;
 }
 
-int main() {
+void VectorPointerTest(vector<int> *v)
+{
+    cout << "This is the memory address: "<< v << std::endl;
+    cout << "Printing out contents of vector of size: " << (*v).size() << "\n";
+    for (auto i : *v){
+        cout << i << std::endl;
+    }
+    (*v)[1] = 30;
+}
+
+int main()
+{
     int num = 2;
-    int * ptr_to_num = &num; //accessing memory address with the amp
-    cout << "Memory reference of num: " << &num << std::endl;
-    PointerTest(ptr_to_num);
-    cout << "The value of num now is: " << num << std::endl;
+    int *ptr_to_num = &num; //accessing memory address with the amp
+    //cout << "Memory reference of num: " << &num << std::endl;
+    //PointerTest(ptr_to_num);
+    //cout << "The value of num now is: " << num << std::endl;
+    vector<int> numList {1,2,3};
+    VectorPointerTest(&numList);
+    cout << "Vector after calling function:\n";
+    for (auto i : numList) {
+        cout << i << std::endl;
+    }
 }
